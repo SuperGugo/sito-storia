f4c66503808c324a3839230e50d43a81f60784d1
SuperGugo <supergugo@protonmail.com>
2026-01-10 17:46:44 +0100

    cleanup

--- src/components/Source.jsx
+++ src/components/Source.jsx
@@ -1,8 +1,8 @@
 export default function Source({ link, image }) {
     return (
         <>
-            <div className="mb-5 mr-5 inline-flex w-64 h-40 items-center justify-center shrink-0 shadow">
-                <a target="_blank" href={`${link}`}><img alt="" src={`${image}`} className="h-40 w-64 rounded-xl" /></a>
+            <div className="mr-5 inline-flex w-64 h-48 items-center justify-center shrink-0 shadow">
+                <a target="_blank" href={`${link}`}><img alt="" src={`${image}`} className="h-48 w-64 rounded-xl" /></a>
             </div>
         </>
     )
